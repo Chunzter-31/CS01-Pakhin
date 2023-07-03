7962a1166a892787d8fd92926a48dd9ab7586f1f
Chunzter-31 <136870069+Chunzter-31@users.noreply.github.com>
2023-07-03 15:48:19 +0700

    Create grading.py

---
+++ grading.py
@@ -0,0 +1,24 @@
+keep = int(input("กรุณาใส่คะแนนเก็บ :"))
+mid = int(input("กรุณาใส่คะแนนกลางภาค :"))
+final = int(input("กรุณาใส่คะแนนปลายภาค :"))
+score = keep+mid+final
+if score >= 80:
+  grade = "A"
+elif score >= 75:
+  grade = "B+"
+elif score >= 70:
+  grade = "B"
+elif score >= 65:
+  grade = "C+"
+elif score >= 60:
+  grade = "C"
+elif score >= 65:
+  grade = "D+"
+elif score >= 50:
+  grade = "D"
+else :
+  grade = "F"
+if score > 100 :
+  print("คะแนนไม่ถูกต้อง")
+if score <= 100 :
+  print("เกรดของคุณคือ :", grade)
